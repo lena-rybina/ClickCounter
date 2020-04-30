ccfc7986c9738c1ea202e681fbc78ef94111b4d4
lena-rybina <ielena.rybina@gmail.com>
2020-04-30 16:41:27 -0400

    Update ViewController.swift

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -12,9 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let label = UILabel ()
+        label.frame = CGRect (x: 150, y: 150, width: 60, height: 60)
+        label.text = "0"
+        
     }
-
-
 }
 
